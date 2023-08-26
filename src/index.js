7a47b2bfe5fb86bf9fb3972fb66b15299f442f00
Dipesh Shah <deepace780@gmail.com>
2023-08-26 09:39:09 +0545

    added react toast to app

--- src/index.js
+++ src/index.js
@@ -7,10 +7,23 @@ import route from "./route";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { ToastContainer } from "react-toastify";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <RouterProvider router={route} />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable={false}
+      pauseOnHover={false}
+      theme="light"
+      style={{ zIndex: 9999999 }}
+    />
   </Provider>
 );
 
