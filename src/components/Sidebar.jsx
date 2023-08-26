016342192cbafbe983833cc16c2b3434bdc655d1
Dipesh Shah <deepace780@gmail.com>
2023-08-26 08:47:11 +0545

    ui fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <>
-      <div className=" flex flex-col justify-center items-center bg-slate-100 p-6 ">
+      <div className=" flex flex-col items-center bg-slate-100 p-6 h-screen ">
         <h1 className="font-bold text-4xl"> Amnil </h1>
         <h1 className="font-bold text-3xl mb-1">Technologies</h1>
         <hr className="bg-black w-[100%] h-1 mb-8" />
